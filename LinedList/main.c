0e3b2a23e0efa7e494a8b5240c7ca972e35248ab
Charles Wang <Charles@wangzengkangdemacbook-pro.local>
2016-02-29 07:33:41 +0800

    Initial Commit

---
+++ LinedList/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LinedList
+//
+//  Created by Charles Wang on 16/2/29.
+//  Copyright © 2016年 CHW. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
