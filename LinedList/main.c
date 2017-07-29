4433644e2be6061a5e9e960309b076752de7ce45
smallX <xieyu559@163.com>
2017-07-29 12:28:56 +0800

    add files

--- LinedList/main.c
+++ LinedList/main.c
@@ -7,9 +7,60 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void)
+{
+    struct node *head, *p = NULL, *q = NULL, *t = NULL;
+    int i, n, a;
+    scanf("%d", &n);
+    head = NULL;
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d", &a);
+        p = (struct node *)malloc(sizeof(struct node));
+        p->data = a;
+        p->next = NULL;
+        
+        if (head == NULL)
+        {
+            head = p;
+        }
+        else
+        {
+            q->next = p;
+        }
+        q = p;
+    }
+
+    scanf("%d", &a);
+    t = head;
+    while (t != NULL)
+    {
+        if (t->next == NULL || t->next->data > a)
+        {
+            p = (struct node *)malloc(sizeof(struct node));
+            p->data = a;
+            p->next = t->next;
+            t->next = p;
+            break;
+        }
+        t = t->next;
+    }
+    
+    t = head;
+    while (t != NULL)
+    {
+        printf("%d ", t->data);
+        t = t->next;
+    }
+    
+    free(p);
+    getchar();getchar();
     return 0;
 }
